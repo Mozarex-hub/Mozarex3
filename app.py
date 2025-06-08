523e2f95cee7e886d5975ab33e547606d12b71bd
Mozarex-hub <mozame@gmail.com>
2025-06-08 22:52:06 +0330

    Update app.py

--- app.py
+++ app.py
@@ -42,4 +42,4 @@ def predict():
         return render_template('result.html', age=age, filename=filename)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
